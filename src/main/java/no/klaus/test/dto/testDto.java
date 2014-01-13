65f2b73123dff87e7b92b11770765eda62836bed
Klaus Torhaugen <kla@vps.no>
2014-01-13 07:26:34 +0100

    Dto endringer

--- src/main/java/no/klaus/test/dto/testDto.java
+++ src/main/java/no/klaus/test/dto/testDto.java
@@ -8,6 +8,29 @@ public class TestDto {
     public String forNavn;
     public String etterNavn;
 
+    public String getKontohaverId() {
+        return kontohaverId;
+    }
+
+    public void setKontohaverId(String kontohaverId) {
+        this.kontohaverId = kontohaverId;
+    }
+
+    public String getForNavn() {
+        return forNavn;
+    }
+
+    public void setForNavn(String forNavn) {
+        this.forNavn = forNavn;
+    }
+
+    public String getEtterNavn() {
+        return etterNavn;
+    }
+
+    public void setEtterNavn(String etterNavn) {
+        this.etterNavn = etterNavn;
+    }
 
     public String toString(){
         return "Kontohaver ID: " + kontohaverId + " Navn: " + forNavn + " " + etterNavn;
